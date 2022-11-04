2fa9fb9478351d5db103f5c8bb28cf2e15c4a2bd
monicabarreto <monicabarreto88gm@gmail.com>
2022-11-04 14:21:07 -0300

    mudanças no css e html

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const wraper = document.querySelector(".sliderwrapper")
-const menuitems = document.querySelectorAll(".menuitem")
 
-menuitems.forEach((item,index) =>{
-    item.addEventListener("click", ()=>{
-        wraper.style.transform = `translateX(${-100 * index}vw)`
-       
-    })
 
-})
 
